a802d243479dabda9e021758adb512619497cad6
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-03-10 11:47:59 +0100

    Update 04b_csv_parsing.sql

--- 04_SQL_DataImport/04b_csv_parsing.sql
+++ 04_SQL_DataImport/04b_csv_parsing.sql
@@ -2,3 +2,13 @@
 
 USE work;
 
+
+/* Eingrenzen/Filtern durch WHERE */
+/* UPDATE */
+#UPDATE cats SET cat_name="Alonzo"; # Achtung, ALLES wird überschrieben!
+UPDATE cats SET cat_name="Alonzo" WHERE id = 2; #Sicher!
+#UPDATE cats SET cat_name="Alonzo" WHERE fur_color = "grey"; #Unsicher!
+
+/* READ - SELECT */
+
+SELECT * FROM cats;
