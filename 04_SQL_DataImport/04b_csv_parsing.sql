58fdb491673ab344b846264e3ebf1ce7e39fbc93
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-03-10 12:10:04 +0100

    Update 04b_csv_parsing.sql

--- 04_SQL_DataImport/04b_csv_parsing.sql
+++ 04_SQL_DataImport/04b_csv_parsing.sql
@@ -10,5 +10,18 @@ UPDATE cats SET cat_name="Alonzo" WHERE id = 2; #Sicher!
 #UPDATE cats SET cat_name="Alonzo" WHERE fur_color = "grey"; #Unsicher!
 
 /* READ - SELECT */
+#SELECT * FROM cats; #gesamte Tabelle
+#SELECT cat_name FROM cats; #Feld
+#SELECT cat_name,cat_age FROM cats; #2 Felder kombiniert
+#SELECT cat_age,cat_name FROM cats; #2 Felder kombiniert
+#SELECT cat_age AS "Alter", cat_name AS "Name" FROM cats; #2 Felder / neue Namen
+
+SELECT #Restriktion durch WHERE
+	cat_age AS "Alter", cat_name AS "Name" 
+FROM cats 
+#WHERE id = 1  #id
+WHERE fur_color = "red" #fur_color
+;
+
 
 SELECT * FROM cats;
