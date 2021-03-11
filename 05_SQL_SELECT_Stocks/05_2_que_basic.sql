28ebd53829b6dfe2af0efe1add9bd1f6551d3007
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-03-11 09:31:00 +0100

    Update 05_2_que_basic.sql

--- 05_SQL_SELECT_Stocks/05_2_que_basic.sql
+++ 05_SQL_SELECT_Stocks/05_2_que_basic.sql
@@ -19,11 +19,24 @@ Ausgabe beschränken
 #SELECT * FROM stocks.ccc_list LIMIT 10,5;
 
 -- Abfrage eines Feldes oder einer Feld-Kombi (rows 1-10 )
-SELECT 
+/*SELECT 
 industry,ticker,c_name
 FROM stocks.ccc_list 
-LIMIT 10; 
+LIMIT 10; */
 
+-- Abfrage eines Feldes oder einer Feld-Kombi m. Umbenennung (Alias)
+/*SELECT 
+ticker AS "SYMBOL",
+c_name AS "Unternehmen",
+price AS "Kurs in $"
+FROM stocks.ccc_list 
+LIMIT 10; */
 
-
-
+-- Kombinationen aus mehreren Feldern in neues Feld 
+-- hier mit Verknüpfung CONCAT() // String-Fkt.
+SELECT 
+ticker "SYMBOL",
+c_name "Unternehmen",
+CONCAT(sector," | ",industry) "Operations"
+FROM stocks.ccc_list 
+LIMIT 5; 
