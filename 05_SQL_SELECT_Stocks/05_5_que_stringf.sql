5b1f2de968c5e35a55a992f6bad9063d9df27109
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-03-12 08:23:46 +0100

    Update 05_5_que_stringf.sql

---
+++ 05_SQL_SELECT_Stocks/05_5_que_stringf.sql
@@ -0,0 +1,14 @@
+/* String-Funktionen */
+
+USE stocks;
+
+/*
+    CONCAT() / Verknuepfung "string" + "string" ...
+    UPPER() --> alles in Großbuchstaben
+    LOWER() --> alles in Kleinbuchstaben
+    LENGTH() --> String-Laenge
+    REPLACE() --> Ersetzen von Stringanteilen
+    TRIM() --> Trimmen vor/nach String (Whitespace)
+*/
+
+-- CONCAT() / Verknuepfung "string" + "string" ....
