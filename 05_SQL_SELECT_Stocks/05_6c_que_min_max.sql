994bc68bcb947e5d98d38eaf30273f3331197d52
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-03-12 14:18:38 +0100

    Update 05_6c_que_min_max.sql

--- 05_SQL_SELECT_Stocks/05_6c_que_min_max.sql
+++ 05_SQL_SELECT_Stocks/05_6c_que_min_max.sql
@@ -1,3 +1,25 @@
 /* Aggregationen MIN/MAX */
 
 USE stocks;
+
+-- Preis der teuersten/günstigsten Aktie?
+SELECT
+	max(price) AS "Max. Preis"
+    #min(price) AS "Min. PREIS"
+FROM stocks.ccc_list
+;
+
+# SELECT max(price) FROM stocks.ccc_list;
+# SELECT min(price) FROM stocks.ccc_list;
+
+-- WELCHE Aktie ist am teuersten/günstigsten ?
+-- 1. Subquery
+
+SELECT
+	ticker SYM,
+    c_name Unternehmen,
+    price Aktienkurs
+FROM stocks.ccc_list
+#WHERE price = (SELECT max(price) FROM stocks.ccc_list)  # Subquery / Klammern!
+WHERE price = (SELECT min(price) FROM stocks.ccc_list) 
+;
