859308d57d05b2cfd4fe61873734d8dad91694ee
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-03-12 10:16:23 +0100

    distinct++

---
+++ 05_SQL_SELECT_Stocks/05_6a_que_distinct.sql
@@ -0,0 +1,20 @@
+/* DISTINCT */
+
+USE stocks;
+
+-- alle Sektoren
+/*SELECT
+	sector
+FROM stocks.ccc_list
+ORDER BY sector ASC
+LIMIT 100;*/
+
+-- nur das erste Auftreten von Sektoren
+-- welche VERSCHIEDENEN Sektoren gibt es?
+SELECT
+	DISTINCT sector Industriesektoren
+FROM stocks.ccc_list
+ORDER BY sector ASC
+;
+
+-- welche VERSCHIEDENEN Branchen gibt es?
