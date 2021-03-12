f4b6a8caea5522d196ede0c35fd1386ea779dcc8
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-03-12 10:34:13 +0100

    Create 05_6b_que_count.sql

---
+++ 05_SQL_SELECT_Stocks/05_6b_que_count.sql
@@ -0,0 +1,12 @@
+/* Aggregationen COUNT */
+
+USE stocks;
+
+/* Links:
+    https://dev.mysql.com/doc/refman/5.6/en/counting-rows.html
+    https://dev.mysql.com/doc/refman/5.6/en/group-by-handling.html
+    https://dev.mysql.com/doc/refman/5.6/en/aggregate-functions.html#function_count-distinct
+*/
+
+-- Welche VERSCHIEDENEN Sektoren ?
+-- Wieviele VERSCHIEDENE Sektoren ?
