ad651ee4c67355152bea799ac097d6a18e3afe17
Peter Schmidt <schmidt.multimedia@gmail.com>
2021-03-15 09:07:34 +0100

    Update 05_6d_que_avg.sql

--- 05_SQL_SELECT_Stocks/05_6d_que_avg.sql
+++ 05_SQL_SELECT_Stocks/05_6d_que_avg.sql
@@ -2,3 +2,21 @@
 
 USE stocks;
 
+-- Durchschnittspreis einer Aktie / Banken?
+SELECT
+	round(avg(price),2) Durchschnittspreis  -- Abgerundet / 2 Nachkommastellen
+FROM stocks.ccc_list
+WHERE industry = "Banks"
+#WHERE industry = "Media"
+;
+
+-- KOMBINATION Preis/Dividende pro Industriesektor
+SELECT
+	sector AS Industriesektor,
+    round(avg(price),2) AS Durchschnittspreis,  
+	avg(dividend) AS Durchschnittsdividende
+FROM stocks.ccc_list
+GROUP BY sector
+#ORDER BY avg(price) DESC
+ORDER BY avg(dividend) DESC
+;
